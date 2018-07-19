e33ca55330a0ffab0796bf0af9bb46c59f1d1f81
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-19 14:31:54 -0400

    done

--- src/components/HogIndex.js
+++ src/components/HogIndex.js
@@ -25,6 +25,21 @@ class HogIndex extends Component {
     let hogsArray;
     if (this.state.filtered){
       hogsArray = this.greasedHogs()
+      if (this.state.sort === "Name"){
+        hogsArray = hogsArray.sort( function(a, b) {
+          var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+          if (nameA < nameB)
+              return -1
+          if (nameA > nameB)
+              return 1
+          return 0
+        })
+      } else if (this.state.sort === "Weight"){
+        hogsArray = hogsArray.sort( function(a, b) {
+        return a["weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"] - b["weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"]
+        })
+      console.log(hogsArray)
+      }
     }else {
       hogsArray = hogs;
       if (this.state.sort === "Name"){
