24b0e54d2e6ddcee63f00a3d34dc9f895e6b43d7
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-19 17:27:54 -0400

    initial reset

--- src/components/OptionNav.js
+++ src/components/OptionNav.js
@@ -1,43 +1,10 @@
 import React, {Component} from 'react'
 
 class OptionNav extends Component {
-	constructor (props) {
-    super(props)
-    this.state = {
-      greasedFilter: false,
-      sort: "",
-    }
-  }
 
-  handleClick = (event) => {
-    this.setState({
-      selectedHog: event.target.dataset.selectedhogname,
-    })
-  }
-
-  handleFilter = () => {
-    let greasedVar = !this.state.greasedFilter
-    this.setState({
-      greasedFilter: greasedVar
-    })
-  }
-  
-  render() {
-  	return (
-      <div>
-        <label> Greased?
-          <input className="filterButton" type="checkbox" checked={this.state.greased} onChange={this.handleFilter} />
-        </label>
-        <label> Sort
-          <select value={this.state.sort} className="filterButton" onChange={this.handleSelect}>
-            <option value=""></option>
-            <option value="Name">Name</option>
-            <option value="Weight">Weight</option>
-          </select>
-        </label>
-      </div>
-	  )
-  }
+render () {
+  return <div>"hi!"</div>
+}
 }
 
 export default OptionNav;
